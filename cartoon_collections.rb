60df8c6afd6308f7c86cd57fd36e0e0a98ab1801
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-11 03:09:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def long_planeteer_calls(words)
   words.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
